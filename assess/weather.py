da8128ada51c633549b26aa49520d1e5ce0c26e4
Heaven.Xie <xieheaven@icloud.com>
2023-02-15 23:20:44 +0800

    新增 天气
    完成设计说明，尚未实现

---
+++ assess/weather.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+"""
+@File    ：weather.py
+@Author  ：heaven
+@Date    ：2023/2/15 22:49
+
+假设：
+望远镜是地平式的，HEALPix 也用地平的球，天区云层覆盖情况，也是地平坐标系
+HEALPix 的`nside`取决于望远镜的视场。我们假设，望远镜的视场是 a_telescope，nside2resol(nside) 应等于 a_telescope
+"""
+
+
