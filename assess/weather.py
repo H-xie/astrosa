56afdba3b305f4b20398c397156d9f9cc8636632
Heaven Xie <heavenxie@yeah.net>
2023-02-16 19:43:02 +0800

    增加天气的实现

--- assess/weather.py
+++ assess/weather.py
@@ -8,5 +8,28 @@
 望远镜是地平式的，HEALPix 也用地平的球，天区云层覆盖情况，也是地平坐标系
 HEALPix 的`nside`取决于望远镜的视场。我们假设，望远镜的视场是 a_telescope，nside2resol(nside) 应等于 a_telescope
 """
+from astropy.time import Time
+import pandas as pd
 
 
+class Cloud(object):
+    def __init__(self, data: pd.DataFrame):
+        self.data = data
+
+    def __getitem__(self, item):
+        return self.data.at[item, "cloud"]
+
+
+class Weather(object):
+    """一个时刻的天气情况
+    """
+
+    def __init__(self, datetime: Time, cloud: pd.DataFrame):
+        """cloud is in index of healpix_idx
+        """
+        self.datetime = datetime
+        self._cloud = Cloud(cloud)
+
+    @property
+    def cloud(self):
+        return self._cloud
