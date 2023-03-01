11b2095bed8d827d0a32a91d620d2c889c0f324b
Heaven.Xie <xieheaven@icloud.com>
2023-03-01 23:11:06 +0800

    Add healpix wrap
    Wrapping [astropy-healpix](https://astropy-healpix.readthedocs.io/en/latest/coordinates.html)
    是一个正在开发的库。为了接口的稳定，我需要做个封装

--- cook_weather.py
+++ cook_weather.py
@@ -1,30 +1,30 @@
-import pandas as pd
-import numpy as np
-import healpy as hp
-from astropy.time import Time
 import astropy.units as u
+import numpy as np
+import pandas as pd
+# import healpy as hp
+from astropy.time import Time
+
+from healpix import HH
 
 # create a zone
 nside = 4 ** 2
 
-npix = hp.nside2npix(nside)
+npix = HH.nside2npix(nside)
 
 print(npix)
 
 # set random cloud
 # cloud is (0,1) means the thickness
 
-ntime_tic = 1000 # every minute
+ntime_tic = 1000  # every minute
 
-cloud = np.random.random([ ntime_tic,npix])
+cloud = np.random.random([ntime_tic, npix])
 print(cloud.shape)
 
 # create data frame
 
 start_time = Time("2023-01-01 19:00:00")
-datetime_list = [start_time + i*u.minute for i in range(ntime_tic)]
+datetime_list = [start_time + i * u.minute for i in range(ntime_tic)]
 
-
-df_cloud = pd.DataFrame(cloud,index=datetime_list, columns=range(npix))
-df_cloud.to_json("assess/tests/data/cloud.json")
-
+df_cloud = pd.DataFrame(cloud, index=datetime_list, columns=range(npix))
+df_cloud.to_json("assess/tests/data/cloudx.json")
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from assess import *
+from rich.progress import Progress, TimeElapsedColumn, track
 
 import astroplan
+from assess import *
+from utils import df2Targets
 
 # scheduler = Scheduler()
 
@@ -9,6 +11,20 @@ s = list()
 s.append(Shot(FixedTarget.from_name('Vega'), Time('2023-01-01 19:00:00'), Time('2023-01-01 19:01:00')))
 s.append(Shot(FixedTarget.from_name('Vega'), Time('2023-01-01 19:02:00'), Time('2023-01-01 19:03:00')))
 s.append(Shot(FixedTarget.from_name('Vega'), Time('2023-01-01 19:06:00'), Time('2023-01-01 19:07:00')))
+
+asp_priority_plan = pd.read_json('assess/tests/data/schedule_140of300.json')
+
+# DataFrame 2 plan
+for _, star in track(asp_priority_plan.iterrows()):
+    star_target = df2Targets(star)
+    start = Time(star['start'])
+    end = Time(star['end'])
+
+    vt_mag = star['VTmag'] * u.mag
+
+    shot = Shot(star_target, start, end)
+
+    break  # for debug
 
 plan = Plan()
 
