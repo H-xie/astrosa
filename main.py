1bd58ff479b827d0b91983440dd1499ead054f62
Heaven Xie <heavenxie@yeah.net>
2023-06-19 10:25:13 +0800

    Merge main
    用 sqlite 数据库取代 json

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 import astroplan
 from ossaf.assess import *
 from utils import obs_start, obs_end, observer
@@ -5,22 +7,26 @@ from utils import obs_start, obs_end, observer
 # scheduler = Scheduler()
 
 # create Plan
-# asp_priority_plan = pd.read_json('ossaf/data/schedule_15of30.json')
-asp_priority_plan = pd.read_json('schedule_167of1000.json')
+conn = sqlite3.connect('ossaf/data/tyc2.sqlite')
+asp_priority_plan = pd.read_sql('select * from priority_schedule_2023_06_08_00_00_00', con=conn)
 asp_priority_plan['start'] = asp_priority_plan['start'].astype('datetime64')
 asp_priority_plan['end'] = asp_priority_plan['end'].astype('datetime64')
 
 asp_priority_plan = asp_priority_plan.rename(columns={'name': 'id',
                                                       'start': 'start_time',
                                                       'end': 'end_time',
-                                                      '_RAJ2000': 'ra',
-                                                      '_DEJ2000': 'dec',
+                                                      'RA_ICRS_': 'ra',
+                                                      'DE_ICRS_': 'dec',
                                                       'VTmag': 'mag'})
 
 plan = Plan(asp_priority_plan)
 
 # read Cloud
-cloud = pd.read_json("ossaf/data/cloud.json", orient='index')
+cloud = pd.read_sql('select * from clear', con=conn, index_col='index')
+cloud.iloc[:, 1::] = 1 - cloud.iloc[:, 1::]
+cloud.index = cloud.index.astype('datetime64')
+cloud = cloud.iloc[cloud.index.argsort()]
+cloud.columns = cloud.columns.astype(int)
 weather = Weather(Cloud(cloud))
 
 ossaf = Ossaf(observer, plan, None, weather, obs_start=obs_start, obs_end=obs_end)
@@ -29,6 +35,6 @@ result = ossaf.run()
 
 print(result)
 
-result['score'].to_csv('score167.csv')
+result['score'].to_csv('score.csv')
 
 print("end")
